2a47270eb775e1c0d57c086bc417ae5808a39534
Student Administrator <aoun.rony@gmail.com>
2017-11-30 11:55:13 -0500

    crm integration work

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -22,5 +22,7 @@ namespace student8.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<student8.Models.Inquiry> Inquiry { get; set; }
     }
 }
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace student8.Models
 {
@@ -11,5 +12,16 @@ namespace student8.Models
     {
         public string FirstName { get; set;}
         public string LastName { get; set; }
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+            set
+            {
+            }
+        }
     }
 }
